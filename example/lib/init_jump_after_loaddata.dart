a0d7c684972b0759131de8140e66e30cc5e29425
robert <robert@bitguild.com>
2022-03-25 13:51:11 +0800

    Update init_jump_after_loaddata.dart

--- example/lib/init_jump_after_loaddata.dart
+++ example/lib/init_jump_after_loaddata.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 
-import 'package:flutter_list_view/flutter_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_list_view/flutter_list_view.dart';
 
 class InitJumpAfterLoadDataPage extends StatefulWidget {
   const InitJumpAfterLoadDataPage({Key? key}) : super(key: key);
@@ -13,15 +13,46 @@ class InitJumpAfterLoadDataPage extends StatefulWidget {
 }
 
 class _InitJumpAfterLoadDataPageState extends State<InitJumpAfterLoadDataPage> {
-  List<int> data = [];
+  List<String> data = [];
+  final _random = Random();
+  FlutterListViewController viewController = FlutterListViewController();
+  int initIndex = 90;
+  double initOffset = 0;
+  List<FlutterListViewItemPosition> lastPositions = [];
+  String htmlLine = "<div>Single line</div>";
+
   @override
   initState() {
     super.initState();
+
+    viewController.sliverController.onPaintItemPositionsCallback =
+        (height, positions) {
+      lastPositions = positions;
+    };
+
+    for (var i = 0; i < 100; i++) {
+      data.add("<div><b>$i</b></div>" +
+          List.filled(2 + _random.nextInt(10), '\n').join(htmlLine));
+    }
   }
 
   @override
   dispose() {
     super.dispose();
+  }
+
+  void _insertDataAndRestoreSaveScrollPosition() {
+    initIndex = lastPositions[1].index;
+    initOffset = lastPositions[1].offset;
+
+    List<String> newData = [];
+    newData.addAll(data);
+    newData.insert(
+        0, List.filled(2 + _random.nextInt(10), '\n').join(htmlLine));
+    data = newData;
+    initIndex++;
+    print("---------------index:$initIndex, offset: $initOffset");
+    setState(() {});
   }
 
   @override
@@ -34,36 +65,30 @@ class _InitJumpAfterLoadDataPageState extends State<InitJumpAfterLoadDataPage> {
         children: [
           Wrap(spacing: 20, children: [
             ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    for (var i = 0; i < 1000; i++) {
-                      data.add(i);
-                    }
-                  });
-                },
-                child: const Text("Load data")),
+                onPressed: _insertDataAndRestoreSaveScrollPosition,
+                child: const Text("Insert line at index 0")),
           ]),
           Expanded(
-            child: FlutterListView(
+            child: Scrollbar(
+              child: FlutterListView(
+                scrollDirection: Axis.vertical,
+                key: ObjectKey(data.hashCode),
+                controller: viewController,
                 delegate: FlutterListViewDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                    color: Colors.white, child: Html(data: """<div>$index:
-        <h1>Demo Page</h1>
-        <p>This is a fantastic product that you should buy!</p>
-        <h3>Features</h3>
-        <ul>
-          ${"<li>Item data</li>" * ((index + 1) % 5)}
-        </ul>
-      </div>"""));
-              },
-              preferItemHeight: 200,
-              childCount: data.length,
-              initIndex: 400,
-              initOffset: 40,
-              // initOffsetBasedOnBottom: false,
-              firstItemAlign: FirstItemAlign.end,
-            )),
+                  (BuildContext context, int index) {
+                    return Container(
+                      color: Colors.white,
+                      child: Html(data: data[index]),
+                    );
+                  },
+                  preferItemHeight: 200,
+                  childCount: data.length,
+                  initIndex: initIndex,
+                  initOffset: initOffset,
+                  firstItemAlign: FirstItemAlign.end,
+                ),
+              ),
+            ),
           ),
         ],
       ),
